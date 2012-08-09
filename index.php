e7740443dbb309ebb0887e8e44a61cf8f8e38f8a
Yandorf Xin <yandorf@gmail.com>
2012-08-09 15:09:37 -0500

    html base
    html y css base

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>GitHub</title>
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+<?php 
+	date_default_timezone_set('ETC/GMT-7');
+?>
+	<header>
+		<hgroup>
+    		<h1>Gianino Cole</h1>
+    		<h2>Web Developer (?)</h2>
+  		</hgroup>
+		<nav></nav>
+	</header>
+	<section>
+		<article>
+			<p>Este es mi primer archivo en GitHub</p>
+			<p>Son las : <?php echo date('h:i:s'); ?></p>
+		</article>
+	</section>
+</body>
+</html>
